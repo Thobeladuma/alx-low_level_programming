4c75da8b5170553e9ee6c3f1624a33c0528eaa33
Thobeladuma <thobeladuma1@gmail.com>
2023-02-23 03:54:56 -0800

    updated commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isupper - A function that checks for uppercase character.
  * @c: An input character
- * Return: 1 if c is uppercase or 0 otherwise
+i * Return: 1 if c is uppercase or 0 otherwise
  */
 int _isupper(int c)
 {
